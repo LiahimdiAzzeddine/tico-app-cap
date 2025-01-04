984b6f0d09768b71bfadfb3bc8c0612b85387821
= <=>
2025-01-04 17:22:05 +0100

    commit

--- src/composants/navBars/BottomNavbar.jsx
+++ src/composants/navBars/BottomNavbar.jsx
@@ -59,7 +59,7 @@ const BottomNavbar = () => {
     >
       <IonToolbar
         className="bg-[#f7f7f700] toolbar-custom"
-        style={{ "--ion-toolbar-background": "#ffff" }}
+        style={{ "--ion-toolbar-background": "#ffff",'--padding-bottom':'var(--ion-safe-area-bottomaz, 0.75rem)' }}
         
       >
         <div className="flex justify-around items-center pt-3 px-2 some-child-element">
